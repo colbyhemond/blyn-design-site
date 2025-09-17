3f525d6668c98a43d56384cea9171d6d961e9c11
Colby Hemond <hemond.colby@gmail.com>
2025-09-17 03:56:36 -0400

    update

--- src/app/sitemap.js
+++ src/app/sitemap.js
@@ -13,20 +13,45 @@ export default async function sitemap() {
       lastModified: new Date(),
     },
     {
-      url: `https://${process.env.NEXT_PUBLIC_DOMAIN}/blog`, // Replace with your blog page
+      url: `https://${process.env.NEXT_PUBLIC_DOMAIN}/services`, // Replace with your blog page
       lastModified: new Date(),
-    }
+    },
+    {
+      url: `https://${process.env.NEXT_PUBLIC_DOMAIN}/projects`, // Replace with your blog page
+      lastModified: new Date(),
+    },
+    {
+      url: `https://${process.env.NEXT_PUBLIC_DOMAIN}/contact`, // Replace with your blog page
+      lastModified: new Date(),
+    },
   ]
 
 
-    const POSTS_QUERY = `*[ _type == "post" && defined(slug.current)]|order(publishedAt desc)[0...12]{slug, publishedAt}`;
-    const posts = await client.fetch(POSTS_QUERY);
-    posts.forEach((post) => {
+    // const POSTS_QUERY = `*[ _type == "post" && defined(slug.current)]|order(publishedAt desc)[0...12]{slug, publishedAt}`;
+    // const posts = await client.fetch(POSTS_QUERY);
+    // posts.forEach((post) => {
+    //     links.push({
+    //         url: `https://${process.env.NEXT_PUBLIC_DOMAIN}/blog/${post.slug.current}`,
+    //         lastModified: new Date(post.publishedAt),
+    //     });
+    // }); 
+
+    const SERVICES_QUERY = `*[_type == "service" && defined(slug.current)]|order(_createdAt desc){slug, _updatedAt}`;
+    const services = await client.fetch(SERVICES_QUERY);
+    services.forEach((service) => {
         links.push({
-            url: `https://${process.env.NEXT_PUBLIC_DOMAIN}/blog/${post.slug.current}`,
-            lastModified: new Date(post.publishedAt),
+            url: `https://${process.env.NEXT_PUBLIC_DOMAIN}/services/${service.slug.current}`,
+            lastModified: new Date(service._updatedAt),
         });
-    }); 
+    });
+    const PROJECTS_QUERY = `*[_type == "project" && defined(slug.current)]|order(_createdAt desc){slug, _updatedAt}`;
+    const projects = await client.fetch(PROJECTS_QUERY);
+    projects.forEach((project) => {
+        links.push({
+            url: `https://${process.env.NEXT_PUBLIC_DOMAIN}/projects/${project.slug.current}`,
+            lastModified: new Date(project._updatedAt),
+        });
+    });
 
 
   return links;
